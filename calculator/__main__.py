b1077358dfe6e92512058bfda75228f6995dacfc
dmytrocash <dmitrycash1932@gmail.com>
2022-06-28 22:47:17 +0300

    Adding 6 files: calc.py with math logic and possibility to reuse, exceptions.py with custom exceptions, test.py with 11 tests, __main__.py which runs the program, .gitignore with .DS_Store and .idea inside and empty __init__.py

---
+++ calculator/__main__.py
@@ -0,0 +1,27 @@
+from calc.exceptions import ZeroDivError, WrongInputError
+from calc.calc import calculator
+
+
+def main():
+    print('Welcome to calculator. You can do operations with two integers and '
+          'followed mathematical signs: +, -, *, /. For exit type "e".')
+    while True:
+        try:
+            char_input = input("Type in the math expression you'd like to "
+                               "complete:").replace(' ', '')
+            if char_input == 'e':
+                return False
+            c = calculator(char_input)
+            print(c)
+            if c is False:
+                break
+        except ZeroDivError:
+            print('Dividing by zero is a silly thing to do!')
+        except WrongInputError:
+            print('Type math expression in right format. E.g. 1 + 99')
+
+
+if __name__ == '__main__':
+    print(main())
+
+# cd /Users/dmytrocash/Desktop/code/playground/calc
