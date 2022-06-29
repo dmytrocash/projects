55aa7f4d492260571dcd86a34bcc19bb2446c87c
dmytrocash <91541394+dmytrocash@users.noreply.github.com>
2022-06-29 15:49:39 +0300

    Create calculator features (#6)
    * Adding 6 files: calc.py with math logic and possibility to reuse, exceptions.py with custom exceptions, test.py with 11 tests, __main__.py which runs the program, .gitignore with .DS_Store and .idea inside and empty __init__.py
    
    * Deleted comment
    
    * Changed text to Python.gitignore
    
    * Replaced .gitignore to root
    
    * Review fixes
    
    * Splited test class into two: positive and negative
    
    * Refactored except block in __main__.py

---
+++ calculator/test.py
@@ -0,0 +1,53 @@
+import unittest
+from calc import calculator
+from exceptions import ZeroDivError, WrongInputError
+
+
+class PositiveTestCase(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(calculator('4+7'), 11)
+
+    def test_subtract(self):
+        self.assertEqual(calculator('10-5'), 5)
+
+    def test_multiply(self):
+        self.assertEqual(calculator('3*7'), 21)
+
+    def test_divide(self):
+        self.assertEqual(calculator('10/2'), 5)
+
+
+class NegativeTestCase(unittest.TestCase):
+
+    def test_division_by_zero(self):
+        with self.assertRaises(ZeroDivError):
+            calculator('10/0')
+
+    def test_symbols(self):
+        with self.assertRaises(WrongInputError):
+            calculator('3%4')
+
+    def test_no_sign(self):
+        with self.assertRaises(WrongInputError):
+            calculator('2 333')
+
+    def test_one_integer(self):
+        with self.assertRaises(WrongInputError):
+            calculator('44/')
+
+    def test_decimal(self):
+        with self.assertRaises(WrongInputError):
+            calculator('2.2+3')
+
+    def test_len(self):
+        with self.assertRaises(WrongInputError):
+            calculator('2+2-1')
+
+    def test_letters(self):
+        with self.assertRaises(WrongInputError):
+            calculator('2+b')
+
+
+if __name__ == '__main__':
+    unittest.main()
