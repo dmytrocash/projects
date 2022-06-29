bfc7688821d6c1246c1aba6c73ada8465304ff25
dmytrocash <dmitrycash1932@gmail.com>
2022-06-29 11:21:51 +0300

    Splited test class into two: positive and negative

--- calculator/test.py
+++ calculator/test.py
@@ -3,45 +3,48 @@ from calc import calculator
 from exceptions import ZeroDivError, WrongInputError
 
 
-class OperationsTestCase(unittest.TestCase):
+class PositiveTestCase(unittest.TestCase):
 
-    def positive_test_add(self):
+    def test_add(self):
         self.assertEqual(calculator('4+7'), 11)
 
-    def positive_test_subtract(self):
+    def test_subtract(self):
         self.assertEqual(calculator('10-5'), 5)
 
-    def positive_test_multiply(self):
+    def test_multiply(self):
         self.assertEqual(calculator('3*7'), 21)
 
-    def positive_test_divide(self):
+    def test_divide(self):
         self.assertEqual(calculator('10/2'), 5)
 
-    def negative_test_division_by_zero(self):
+
+class NegativeTestCase(unittest.TestCase):
+
+    def test_division_by_zero(self):
         with self.assertRaises(ZeroDivError):
             calculator('10/0')
 
-    def negative_test_symbols(self):
+    def test_symbols(self):
         with self.assertRaises(WrongInputError):
             calculator('3%4')
 
-    def negative_test_no_sign(self):
+    def test_no_sign(self):
         with self.assertRaises(WrongInputError):
             calculator('2 333')
 
-    def negative_test_one_integer(self):
+    def test_one_integer(self):
         with self.assertRaises(WrongInputError):
             calculator('44/')
 
-    def negative_test_decimal(self):
+    def test_decimal(self):
         with self.assertRaises(WrongInputError):
             calculator('2.2+3')
 
-    def negative_test_len(self):
+    def test_len(self):
         with self.assertRaises(WrongInputError):
             calculator('2+2-1')
 
-    def negative_test_letters(self):
+    def test_letters(self):
         with self.assertRaises(WrongInputError):
             calculator('2+b')
 
