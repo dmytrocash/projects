ac151fe067c95ade41d67d84c0222e3eb5f37fa8
dmytrocash <dmitrycash1932@gmail.com>
2022-06-30 18:14:53 +0300

    Edited list-of-strings.py

--- exercises/list-of-strings.py
+++ exercises/list-of-strings.py
@@ -1,18 +1,31 @@
-a = "1+ 22-*/   333+ - 3239 - 23 + 2 - 1 +".replace(' ', '')
+def list_of_strings(input_str):
+    """This function accepts string with integers and symbols and make a list
+    of strings with no spaces out of it.
 
-result = []
-num = ''
-for char in a:
-    if char.isdigit():
-        num = num + char
-    else:
-        if num != '':
-            result.append(num)
-            num = ''
-        if char.isnumeric() is False:
-            result.append(char)
-if num != '':
-    result.append(num)
-print(result)
+    >>> list_of_strings('1+ 22-*/   333+ - 3239 - 23 + 2 - 1 +')
+    ['1', '+', '22', '-', '*', '/', '333', '+', '-', '3239', '-', '23', '+', '2', '-', '1', '+']
 
-# ['1', '+', '22', '-', '*', '/', '333', '+', '-', '3239', '-', '23', '+', '2', '-', '1', '+']
+    >>> list_of_strings('3 + 448 - 2 *        4/3')
+    ['3', '+', '448', '-', '2', '*', '4', '/', '3']
+    """
+    modified_string = input_str.replace(' ', '')
+    result = []
+    num = ''
+    for char in modified_string:
+        if char.isdigit():
+            num = num + char
+        else:
+            if num != '':
+                result.append(num)
+                num = ''
+            if char.isnumeric() is False:
+                result.append(char)
+    if num != '':
+        result.append(num)
+
+    return result
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
