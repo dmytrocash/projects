b8eef1c99cd3bc4e06d4e5ed5b4b902f4685c113
dmytrocash <dmitrycash1932@gmail.com>
2022-07-18 14:13:33 +0300

    Completed parser without nesting

--- metrex-site-parser/src/metrex-site-parser.py
+++ metrex-site-parser/src/metrex-site-parser.py
@@ -3,7 +3,7 @@ import argparse
 import lxml.html
 
 
-metrex_site_url = 'git '
+metrex_site_url = 'https://metrex.kiev.ua'
 currency_url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
 
 
@@ -14,10 +14,27 @@ class Product:
         self.price = product_price
         self.id = product_id
         self.specifications = product_specifications
-        self.related = [Product()]
+        self.related = []
+
+    def add(self, product):
+        self.related.append(product)
 
 
-# class Currency:
+class Currency:
+    def __init__(self, url):
+        self.url = url
+        self.data = None
+
+    def get_data(self):
+        self.data = requests.get(self.url).json()
+
+    def convert(self, to_currency, product_price):
+        if to_currency == "uah":
+            return "{} {}".format(product_price, to_currency.upper())
+        for item in self.data:
+            if item["cc"] == to_currency.upper():
+                return "{} {}".format(round(product_price / item['rate'], 2),
+                                      to_currency.upper())
 
 
 def get_arguments():
@@ -59,13 +76,6 @@ def get_price(product_html):
         '//p/span[@data-qaid="product_price"]/text()')).replace(',', '.'))
 
 
-def currency_converter(currency_code, product_price):
-    currency_response = requests.get(currency_url).json()
-    for item in currency_response:
-        if item["cc"] == currency_code:
-            return product_price / item['rate']
-
-
 def get_product_specifications(product_html):
     unedited_specifications = product_html.xpath('//tr/td/text()')
 
@@ -95,104 +105,118 @@ def get_product_id(product_html):
 
 def if_product_exists(search_html):
     if search_html.xpath('//h1/span[contains(text(),"0 наименований")]/text()'):
-        return False
-    else:
         return True
+    else:
+        return False
 
 
-def get_info():
-    """Responsible for collecting all product data"""
-
-    search_url = get_search_url(args.product_id)
-    search_html = get_search_html(search_url)
-    product_url = get_product_url(search_html)
+def get_product_info(product_url):
     product_html = get_product_html(product_url)
     product_title = get_product_title(product_html)
-    product_id = get_product_id(product_html)
     product_price = get_price(product_html)
+    product_id = get_product_id(product_html)
     product_specifications = get_product_specifications(product_html)
-    related_products_url = get_related_products_url(product_html)
     product = Product(product_title, product_price, product_id,
                       product_specifications)
+
+    return product, product_html
+
+
+def get_related_products(output_related, product, product_html):
+    related_products_url = get_related_products_url(product_html)
     related_product_html = get_product_html(related_products_url)
     related_urls = related_product_html.xpath(
         '//a[@class="cs-carousel__title"]/@href')
+
     related_products_urls = []
     for related_url in related_urls:
         related_product_url = metrex_site_url + related_url
         related_products_urls.append(related_product_url)
+    selected_urls = related_products_urls[:output_related]
+
+    for item in selected_urls:
+        product_html = get_product_html(item)
+        product_title = get_product_title(product_html)
+        product_id = get_product_id(product_html)
+        product_price = get_price(product_html)
+
+        related_products = Product(product_title, product_price, product_id)
+        product.add(related_products)
+
+    return len(related_products_urls)
 
 
-# get_info()
+def output_products_info(output_price, output_related, output_specifications,
+                         product, len_related_products, output_currency, currency):
+    indent = '    '
+    template = "{}{}: {}{}{}"
+    print(template.format(indent, "Наименование", product.title, "", ""))
+    print(template.format(indent, "Код товара", product.id, "", ""))
+    if output_price:
+        print(template.format(indent, "Цена", currency.convert(
+            output_currency, product.price), "", ""))
 
+    if output_specifications:
+        print(template.format(indent, "Характеристики", "", "\n",
+                              product.specifications))
 
-def main(product_check, product_title, search_html):
-    args = get_arguments()
-    if product_check:
-        product_check = if_product_exists(search_html)
-        return print("Товара с таким артикулом не сущетвует!")
-    else:
-        print(f"Наименование: {product_title}")
-
-
-# main()
-
-
-def product_info(args, product_price, product_specifications,
-                 related_products_urls):
-    if args.price:
-        if args.currency == 'uah':
-            print(f"Цена: {round(product_price, 2)} {args.currency.upper()}")
-        else:
-            print(f"Цена: {round(currency_converter(args.currency.upper()), 2)} {args.currency.upper()}")
-
-    if args.specifications:
-        print(f"Характеристики:\n{product_specifications}")
-
-    if args.related > len(related_products_urls):
-        print(f"Подобных товаров меньше, чем {args.related}"
-              f". Чтобы посмотреть подобные товары, введите число от 1 до "
-              f"{len(related_products_urls)} включительно.")
-    else:
-        print(f"Сопутствующие товары:")
-
-        selected_products = related_products_urls[:args.related]
+    if output_related:
+        print("{}{}".format(indent*2, "Сопутствующие товары:"))
 
         related_products_list = []
-        for product in selected_products:
-            related_product_html = get_product_html(''.join(product))
-            related_product_title = get_product_title(related_product_html)
-            related_product_id = get_product_id(related_product_html)
-            related_product_price = get_price(related_product_html)
-
-            related_products_list.append(
-                f'  - Название: {related_product_title}')
-            related_products_list.append(
-                f'    Код товара: {related_product_id}')
-
-            if args.currency == 'uah':
-                related_products_list.append(f'    Цена: {round(related_product_price, 2)} {args.currency.upper()}')
-            else:
-                rate = currency_converter(args.currency.upper())
-                related_products_list.append(
-                    f'    Цена: {round(rate, 2)} {args.currency.upper()}')
+        for item in product.related:
+            related_products_list.append(template.format(indent*2, "Наименование",
+                                                         item.title, "", ""))
+            related_products_list.append(template.format(indent*2, "Код товара",
+                                                         item.id, "", ""))
+            related_products_list.append(template.format(
+                indent*2, "Цена", currency.convert(
+                    output_currency, item.price), "", ""))
 
         print(*related_products_list, sep='\n')
 
+    elif output_related > len_related_products:
+        print("Подобных товаров меньше, чем {}. Чтобы посмотреть подобные "
+              "товары, введите число от 1 до {} "
+              "включительно.".format(output_related, len_related_products))
 
-product_info()
+
+def main():
+    args = get_arguments()
+    output_price = args.price
+    output_related = args.related
+    output_specifications = args.specifications
+    output_currency = args.currency
+
+    currency = Currency(currency_url)
+    currency.get_data()
+
+    search_url = get_search_url(args.product_id)
+    search_html = get_search_html(search_url)
+    product_url = get_product_url(search_html)
+    if product_url == '':
+        print("Товар с таким артикулом не сущетвует!")
+        return
+    product, product_html = get_product_info(product_url)
+
+    len_related_products = get_related_products(output_related, product,
+                                                product_html)
+
+    output_products_info(output_price, output_related, output_specifications,
+                         product, len_related_products, output_currency, currency)
 
 
-# cd /Users/dmytrocash/Desktop/code/projects-repo
+main()
+
 
 # https://metrex.kiev.ua/site_search?search_term=10602
 # https://metrex.kiev.ua/ua/p1489767212-samorez-dlya-profnastila.html
+
+# cd /Users/dmytrocash/Desktop/code/projects-repo/metrex-site-parser/src
 
 # python3 metrex-site-parser.py 10602 -p -s -r=2
 # python3 metrex-site-parser.py 10602 -p -s -r=4 -c=uah
 # python3 metrex-site-parser.py 10602 -p -s -r=7 -c=usd
 # python3 metrex-site-parser.py 10602 -p -s -r=10 -c=eur
 
-
-
-
+# python3 metrex-site-parser.py 6605 -p -s -r=3 -c=usd
